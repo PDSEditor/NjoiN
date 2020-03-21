a38f547e1d6887f4358cfec51f70491ccbe373fb
Simone61923 <simo61923@hotmail.com>
2020-03-21 17:49:15 +0100

    fix onConnect metod and add element to widget

--- Malnati_Client/main.cpp
+++ Malnati_Client/main.cpp
@@ -7,6 +7,7 @@
 #include <QCommandLineParser>
 #include <QCommandLineOption>
 #include "loginwindow.h"
+#include <QStyle>
 
 
 
@@ -26,10 +27,19 @@ int main(int argc, char *argv[])
     parser.process(a);
 
     LoginWindow *lw = new LoginWindow();
-    const QRect availableGeometry = QApplication::desktop()->availableGeometry(lw);
-    lw->resize(availableGeometry.width() / 2, (availableGeometry.height() * 2) / 3);
-    lw->move((availableGeometry.width() - lw->width()) / 2,
-            (availableGeometry.height() - lw->height()) / 2);
+    //const QRect availableGeometry = QApplication::desktop()->availableGeometry(lw);
+  //  lw->resize(availableGeometry.width() / 2, (availableGeometry.height() * 2) / 3);
+    //lw->move((availableGeometry.width() - lw->width()) / 2,
+          //  (availableGeometry.height() - lw->height()) / 2);
+
+  /*  int width = availableGeometry.width();
+        int height = availableGeometry.height();
+        width *= 0.5; // 90% of the screen size
+        height *= 0.5; // 90% of the screen size
+        QSize newSize( width, height );
+
+        lw->resize(newSize);*/
+
     lw->show();
 
 
