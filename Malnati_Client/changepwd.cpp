e92e397f46d67426e7ed1de7cd83e7886014f2e6
Simone61923 <simo61923@hotmail.com>
2020-04-11 00:49:35 +0200

    Changepwd window

---
+++ Malnati_Client/changepwd.cpp
@@ -0,0 +1,32 @@
+#include "changepwd.h"
+#include "ui_changepwd.h"
+#include <QMessageBox>
+
+ChangePwd::ChangePwd(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ChangePwd)
+{
+    ui->setupUi(this);
+    setWindowTitle("Change Password");
+}
+
+ChangePwd::~ChangePwd()
+{
+    delete ui;
+}
+
+void ChangePwd::on_buttonBox_accepted()
+{
+    QString pwd = ui->lineEdit->text();
+    if ( pwd.size() == 0 ){
+        QMessageBox::warning(this, "PoliDox", "Password not valid");
+    } else {
+        emit pwdUpdate(pwd);
+        this->hide();
+    }    
+}
+
+void ChangePwd::on_buttonBox_rejected()
+{
+    this->hide();
+}
