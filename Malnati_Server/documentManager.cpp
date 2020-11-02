6354fe3a46a1a3a8a9e92b60b31c0c430de77a48
angelofloridia <angelofloridia@yahoo.it>
2020-11-02 15:44:17 +0100

    adesso il file viene salvato il locale sul server (build directory)

--- Malnati_Server/documentManager.cpp
+++ Malnati_Server/documentManager.cpp
@@ -13,21 +13,23 @@ bool DocumentManager::saveToServer(QString documentId, QList<Symbol> &symbols)
         //        QString name = it.getName();
         //        QString creator = it.getCreator();
         //        if((name+'_'+creator) == documentId) {
-        QFile file("/home/pepos/projects/progetto_malnati/Malnati_Server/backup/" + documentId+ ".bin"); //uri? o name? o id?
-        if (!file.open(QFile::WriteOnly | QIODevice::Text)) return false;
+//        QFile file("/home/pepos/projects/progetto_malnati/Malnati_Server/backup/" + documentId+ ".bin"); //uri? o name? o id?
+        QFile file(documentId + ".bin"); //uri? o name? o id?
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) return false;
         QByteArray bArray;
         for(auto i : symbols){
             bArray.append(i.toJson().toBinaryData());
             //            }
         }
-            auto returnValue = file.write(bArray);
+        auto returnValue = file.write(bArray, bArray.size()); //salva nella cartella di build
 
-            if(returnValue == -1)
-                return false;
-            else {
-                file.close();
-                return true;
-            }
+        if(returnValue == -1)
+            return false;
+        else {
+            file.close();
+//            qDebug() << file.fileName();
+            return true;
+        }
             //da sistemare le parentesi poi dovrebbe andare
 //        }
     }
