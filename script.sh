77560781a4b9001e1515a517b2c8c8974aefef6f
angelofloridia <angelofloridia@yahoo.it>
2020-12-07 10:55:06 +0100

    removed old script.sh

--- script.sh
+++
@@ -1,4 +0,0 @@
-terminator --new-tab -e ./build-Malnati_Server-Desktop_Qt_5_15_1_GCC_64bit-Debug/Malnati_Server &
-terminator --new-tab -e ./build-Malnati_Client-Desktop_Qt_5_15_1_GCC_64bit-Debug/textedit&
-terminator --new-tab -e ./build-Malnati_Client-Desktop_Qt_5_15_1_GCC_64bit-Debug/textedit&
-terminator --new-tab -e ./build-Malnati_Client-Desktop_Qt_5_15_1_GCC_64bit-Debug/textedit&
